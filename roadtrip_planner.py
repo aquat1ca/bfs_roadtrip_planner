3cc6200a14b39cf090c10c1e47d4b41cb1e08532
aquat1ca <kevinhe7961@gmail.com>
2025-11-01 12:23:48 -0700

    roadtrip_planner.py

--- roadtrip_planner.py
+++ roadtrip_planner.py
@@ -2,34 +2,6 @@ import streamlit as st
 from collections import deque
 from usstates import usa_map
 from bfs import bfs_path
-import networkx as nx
-import matplotlib.pyplot as plt
-
-
-def draw_graph_with_path(graph_dict, path):
-    G = nx.Graph(graph_dict)
-
-    # layout for nodes
-    pos = nx.spring_layout(G, seed=7)
-
-    plt.figure(figsize=(10, 7))
-    # draw all edges
-    nx.draw_networkx_edges(G, pos, edge_color="lightgray", width=1)
-
-    # draw all nodes
-    nx.draw_networkx_nodes(G, pos, node_size=400, node_color="#E8F0FE", edgecolors="#1A73E8", linewidths=1)
-    nx.draw_networkx_labels(G, pos, font_size=8)
-
-    # highlight the path if it exists
-    if path and len(path) > 1:
-        path_edges = list(zip(path[:-1], path[1:]))
-        nx.draw_networkx_edges(G, pos, edgelist=path_edges, width=3)
-        # start and end pop
-        nx.draw_networkx_nodes(G, pos, nodelist=[path[0]], node_color="#34A853", node_size=500)
-        nx.draw_networkx_nodes(G, pos, nodelist=[path[-1]], node_color="#EA4335", node_size=500)
-
-    st.pyplot(plt.gcf())
-    plt.close()
 
 st.title("Road Trip Planner")
 st.write("Finds the shortest route for your road trip!")
@@ -48,7 +20,7 @@ if st.button("Find Shortest Route"):
             st.success("Shortest Path Found!")
             st.write("Route:")
             st.write(" ➜ ".join(path))
-            draw_graph_with_path(usa_map, path)
+           
         else:
             st.error("No Path Found Between These States")
 
